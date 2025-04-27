7cc958eae72aa42c756480bc1bcb545db517c8c5
nicer111 <huxuanshuo2022@163.com>
2025-04-27 08:39:41 +0800

    新增推进器相关

--- src/Peripheral/propeller/include/propeller/propeller.h
+++ src/Peripheral/propeller/include/propeller/propeller.h
@@ -3,9 +3,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "cusmsg/msg/can/candlc8.hpp"
-#include "cusmsg/msg/propeller/pstatus.hpp"
-#include "cusmsg/msg/propeller/propellerset.hpp"
+#include "cusmsg/msg/candlc8.hpp"
+#include "cusmsg/msg/pstatus.hpp"
+#include "cusmsg/msg/propellerdefine.hpp"
+#include "cusmsg/msg/propellerstruct.hpp"
 #include <vector>
 #include <linux/can/raw.h>
 #include <net/if.h>
@@ -13,8 +14,13 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <thread>
-using  CANDLC8 = cusmsg::msg::can::CANDLC8;
-using  PSTATUS = cusmsg::msg::propeller::PSTATUS;
+
+using  CANDLC8 = cusmsg::msg::CANDLC8;
+using  PSTATUS = cusmsg::msg::PSTATUS;
+using  PSTRUCT = cusmsg::msg::PROPELLERSTRUCT;
+using  PROPELLERDEFINE = cusmsg::msg::PROPELLERDEFINE;
+using std::placeholders::_1;
+
 class PropellerNode : public rclcpp::Node {
 public:
     PropellerNode();
@@ -25,20 +31,33 @@ private:
     void canReceiver();                                               //can接收线程函数
     void statusLoop();                                                //推进器状态循环can指令
 
-    uint16_t  Front_L_P_    = 0x356;
-    uint16_t  Front_R_P_    = 0x357;
-    uint16_t  Behind_L_P_    = 0x358;
-    uint16_t  Behind_R_P_    = 0x359;
-    uint16_t  Left_P_  = 0x34E;
-    uint16_t  Right_P_ = 0x34D;
+    void ctl_callback(const PSTRUCT & msg);                           //推进器设置回调函数
+    //设置can帧
+    void frame_set(struct can_frame & frame,const uint16_t index,const uint16_t command,int value,uint8_t dlc);
+
+    //                                    LEFT     RIGHT     FL       FR       BL        BR
+    uint16_t  Propeller_address_[6] = {0x34E,0x34D,0x356,0x357,0x358,0x359};
+
 
     std::string Cannum_;
     std::thread receiver_thread_;
     int socket_{-1};
+    const uint16_t Torque_C_ = 0x5443;
+    const uint16_t Speed_C_ = 0x5643;
+    const uint16_t GetSpeed_C_ = 0x5156;
+    const uint16_t GetCurrent_C_ = 0x5143;
+    const uint16_t GetVol_C_ = 0x5150;
+    const uint16_t GetTempM_C_ = 0x5154;  //dianji
+    const uint16_t GetTempE_C_ = 0x4551;  //qudongqi
+    const uint16_t ModeQuest_C_ = 0x4D51;
+    const uint16_t ModeLow_C_ = 0x4D4C;
+    const uint16_t ModeMid_C_ = 0x4D4D;
+    const uint16_t ModeHigh_C_ = 0x4D48;
 
-    rclcpp::TimerBase::SharedPtr timerstatus_;
+    rclcpp::TimerBase::SharedPtr timerstatus_;               //定时器专门用来定时发送读取指令
 
-    rclcpp::Publisher<PSTATUS>::SharedPtr publisherpstatus_;
+    rclcpp::Publisher<PSTATUS>::SharedPtr statuspub_;        //推进器状态发布
+    rclcpp::Subscription<PSTRUCT>::SharedPtr pstructsub_;    //推进器设置转速
 };
 
 
--- src/Peripheral/propeller/src/propeller.cpp
+++ src/Peripheral/propeller/src/propeller.cpp
@@ -6,7 +6,9 @@ PropellerNode::PropellerNode()
 {
     getParams();
     setupCanInterface(Cannum_);
-    publisherpstatus_ = this->create_publisher<PSTATUS>("PropellerStatus", 10);
+    statuspub_ = this->create_publisher<PSTATUS>("PropellerStatus", 10);
+    pstructsub_ = this->create_subscription<PSTRUCT>("Propellerctl", 10,
+                               std::bind(&PropellerNode::ctl_callback, this, _1));
     receiver_thread_ = std::thread(&PropellerNode::canReceiver, this);
     timerstatus_ = this->create_wall_timer(
             std::chrono::milliseconds(500),  // 控制读取推进器状态的周期时间
@@ -14,37 +16,42 @@ PropellerNode::PropellerNode()
     );
 }
 
+void PropellerNode::statusLoop()
+{
+
+}
+
 void PropellerNode::getParams()
 {
     if (!this->has_parameter("left_p"))
         this->declare_parameter<std::uint16_t>("left_p", 0x34E);
     else
-        this->get_parameter("left_p", Left_P_);
+        this->get_parameter("left_p", Propeller_address_[PROPELLERDEFINE::LEFT]);
 
     if (!this->has_parameter("right_p"))
         this->declare_parameter<std::uint16_t>("right_p", 0x34D);
     else
-        this->get_parameter("right_p", Right_P_);
+        this->get_parameter("right_p", Propeller_address_[PROPELLERDEFINE::RIGHT]);
 
     if (!this->has_parameter("front_l_p"))
         this->declare_parameter<std::uint16_t>("front_l_p", 0x356);
     else
-        this->get_parameter("front_l_p", Front_L_P_);
+        this->get_parameter("front_l_p", Propeller_address_[PROPELLERDEFINE::FL]);
 
     if (!this->has_parameter("front_r_p"))
         this->declare_parameter<std::uint16_t>("front_r_p", 0x357);
     else
-        this->get_parameter("front_r_p", Front_R_P_);
+        this->get_parameter("front_r_p", Propeller_address_[PROPELLERDEFINE::FR]);
 
     if (!this->has_parameter("behind_l_p"))
         this->declare_parameter<std::uint16_t>("behind_l_p", 0x358);
     else
-        this->get_parameter("behind_l_p", Behind_L_P_);
+        this->get_parameter("behind_l_p", Propeller_address_[PROPELLERDEFINE::BL]);
 
     if (!this->has_parameter("behind_r_p"))
         this->declare_parameter<std::uint16_t>("behind_r_p", 0x359);
     else
-        this->get_parameter("behind_r_p", Behind_R_P_);
+        this->get_parameter("behind_r_p", Propeller_address_[PROPELLERDEFINE::BR]);
 
 
     if (!this->has_parameter("cannum"))
@@ -108,6 +115,112 @@ void PropellerNode::canReceiver()
             //publisher1_->publish(data);
         }
     }
+}
+
+void PropellerNode::ctl_callback(const PSTRUCT & msg)
+{
+    struct can_frame frame{};
+    if(msg.status)
+        frame_set(frame,Propeller_address_[msg.index],Torque_C_,msg.pthrust,8);
+    else
+        frame_set(frame,Propeller_address_[msg.index],Speed_C_,msg.pspeed,8);
+
+    int bytes_sent = write(socket_, &frame, sizeof(frame));
+    if (bytes_sent < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to send CAN frame");
+    } else {
+        //RCLCPP_INFO(this->get_logger(), "CAN frame send success");
+    }
+}
+
+void PropellerNode::frame_set(struct can_frame & frame,const uint16_t index,const uint16_t command,int value,uint8_t dlc)
+{
+    frame.can_id = index;
+    frame.can_dlc = dlc;
+    if(dlc == 8)
+    {
+        frame.data[0] = command >> 8;
+        frame.data[1] = command;
+        frame.data[4] = value >> 24;
+        frame.data[5] = value >> 16;
+        frame.data[6] = value >> 8;
+        frame.data[7] = value;
+    }else if(dlc == 4)
+    {
+        frame.data[0] = command >> 8;
+        frame.data[1] = command;
+    }
+}
+
+void PropellerNode::message_deal(struct can_frame &frame)
+{
+    uint16_t command =0x0000;
+    uint8_t i = 0;
+    uint16_t index = frame.can_id + 128;
+    /*if((msg.index+128) < 0x340)
+    {
+        return;
+    }*/
+    //RCLCPP_INFO(this->get_logger(), "ssss: %X",index);
+    switch(index)
+    {
+        case Propeller_address_[msg.index]:
+            i = 0;
+            break;
+        case V2_P:
+            i = 1;
+            break;
+        case V3_P:
+            i = 2;
+            break;
+        case V4_P:
+            i = 3;
+            break;
+        case Left_P:
+            i = 4;
+            break;
+        case Right_P:
+            i = 5;
+            break;
+        default:
+            break;
+    }
+    command = (((uint16_t)msg.data[0]) << 8) + msg.data[1];
+    //RCLCPP_INFO(this->get_logger(), "dddd: %X %u %u",command,msg.data[0],msg.data[1]);
+    switch(command)
+    {
+        case C_Speed:
+
+            //p_status.speed[i] = msg.data[4] << 24 + msg.data[5] << 16 + msg.data[6] << 8 + msg.data[7];
+            p_status.speed[i] = (int)((msg.data[4] << 24) | (msg.data[5] << 16) | (msg.data[6] << 8) | msg.data[7]);
+            //p_status.speed[i] = (((int16_t)msg.data[6]) << 8) + msg.data[7];
+            publisher1_->publish(p_status);
+            //RCLCPP_INFO(this->get_logger(), "C_Speed: %u %u %u %u",msg.data[4],msg.data[5],msg.data[6],msg.data[7]);
+            //RCLCPP_INFO(this->get_logger(), "C_Speed: %d",p_status.speed[i]);
+            break;
+        case C_Current:
+            p_status.current[i] = ((float)((msg.data[4] << 24) | (msg.data[5] << 16) | (msg.data[6] << 8) | (msg.data[7])))/10.0;
+            //p_status.current[i] = ((float)(((int32_t)msg.data[6] << 8) + msg.data[7]))/10.0 ;
+            publisher1_->publish(p_status);
+            //RCLCPP_INFO(this->get_logger(), "C_Current: %f",p_status.current[i]);
+            break;
+            /*case C_Vol:
+                p_status.vol[i] = ((float)(((int16_t)msg.data[6] << 8) + msg.data[7]))/10.0 ;
+                //RCLCPP_INFO(this->get_logger(), "C_Vol: %f",p_status.vol[i]);
+                break;*/
+        case C_Mtemp:
+            p_status.motortemp[i] = (int32_t)msg.data[7];
+            publisher1_->publish(p_status);
+            //RCLCPP_INFO(this->get_logger(), "C_Mtemp: %d",p_status.motortemp[i]);
+            break;
+        case C_Dtemp:
+            p_status.drivertemp[i] = (int32_t)msg.data[7];
+            publisher1_->publish(p_status);
+            //RCLCPP_INFO(this->get_logger(), "C_Dtemp: %d",p_status.drivertemp[i]);
+            break;
+    }
+
+    //RCLCPP_INFO(this->get_logger(), "command = %X %X %X %X %X %X %X %X %X",command,msg.data[0],msg.data[1],msg.data[2],msg.data[3],msg.data[4],msg.data[5],msg.data[6],msg.data[7]);
 }
 
 int main(int argc, char* argv[]) {
